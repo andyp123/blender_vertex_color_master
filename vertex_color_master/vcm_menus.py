d302c9966521fbbd44d8431df9e4d3bbaf88f6a1
Andrew Palmer <andyp.123@gmail.com>
2019-05-28 15:35:29 +0900

    Move Vertex Color Master UI to VCM Tab of N panel

--- vertex_color_master/vcm_menus.py
+++ vertex_color_master/vcm_menus.py
@@ -34,7 +34,7 @@ class VERTEXCOLORMASTER_PT_MainPanel(bpy.types.Panel):
     bl_idname = 'VERTEXCOLORMASTER_PT_MainPanel'
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category =  'View'
+    bl_category =  'VCM'
     bl_context = 'vertexpaint'
     # bl_options = {'DEFAULT_CLOSED'}
 
